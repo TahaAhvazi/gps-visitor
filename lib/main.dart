9d5b2094d5e7a567fa535d8139b52aeed1584af2
Taha Ahvazi <tahax.dev@gmail.com>
2024-05-14 01:57:16 +0330

    Detect a location start and then create a polyline

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:location/location.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() => runApp(const MyApp());
@@ -17,15 +17,24 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   String lightMapURL =
       'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png';
-  List<LatLng> points = [];
+  List<LatLng> points = [
+    LatLng(31.2990, 48.6613),
+  ];
   SharedPreferences? _prefs;
   late Timer _timer;
+  Location location = Location();
+  LatLng initialCenter =
+      LatLng(31.2990, 48.6613); // Initial center set to Ahvaz city
 
   @override
   void initState() {
     super.initState();
     initializeSharedPreferences();
+    // Load stored location data when the app starts
+    getStoredLocationData();
     startTimer();
+    // Get current location and set it as the initial center
+    getCurrentLocation();
   }
 
   @override
@@ -42,22 +51,19 @@ class _MyAppState extends State<MyApp> {
                 flex: 3,
                 child: FlutterMap(
                   options: MapOptions(
-                    initialCenter: points.isNotEmpty
-                        ? points.last
-                        : const LatLng(31.304292, 48.671116),
-                    initialZoom: 20.0,
+                    center: initialCenter,
+                    zoom: 15.0,
                   ),
                   children: [
                     TileLayer(
-                      urlTemplate:
-                          "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png",
+                      urlTemplate: lightMapURL,
                     ),
                     PolylineLayer(
                       polylines: [
                         Polyline(
                           points: points,
                           color: Colors.red,
-                          strokeWidth: 6,
+                          strokeWidth: 20,
                         ),
                       ],
                     ),
@@ -98,9 +104,11 @@ class _MyAppState extends State<MyApp> {
     // Simulating location data, replace with actual location retrieval logic
     double latitude = 40.7128; // New York City latitude
     double longitude = -74.0060; // New York City longitude
+
     setState(() {
       points.add(LatLng(latitude, longitude));
     });
+
     storeLocationData(latitude, longitude);
   }
 
@@ -108,6 +116,32 @@ class _MyAppState extends State<MyApp> {
     List<String> locationStrings = _prefs?.getStringList('locations') ?? [];
     locationStrings.add('$latitude,$longitude');
     _prefs?.setStringList('locations', locationStrings);
+  }
+
+  void getStoredLocationData() {
+    List<String>? storedLocationStrings = _prefs?.getStringList('locations');
+    if (storedLocationStrings != null) {
+      setState(() {
+        points = storedLocationStrings.map((location) {
+          List<String> coordinates = location.split(',');
+          double latitude = double.parse(coordinates[0]);
+          double longitude = double.parse(coordinates[1]);
+          return LatLng(latitude, longitude);
+        }).toList();
+      });
+    }
+  }
+
+  void getCurrentLocation() async {
+    try {
+      LocationData currentLocation = await location.getLocation();
+      setState(() {
+        initialCenter =
+            LatLng(currentLocation.latitude!, currentLocation.longitude!);
+      });
+    } catch (e) {
+      print('Failed to get current location: $e');
+    }
   }
 
   @override
